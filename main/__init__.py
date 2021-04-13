e1535773fbf7bd723f3a62df30864448ab332327
Ilnur <ilnur5247@gmail.com>
2021-04-13 19:21:40 +0300

    TruthDiagram

--- main/__init__.py
+++ main/__init__.py
@@ -1,143 +1,32 @@
 from tkinter import *
-from tkinter import messagebox
 from main.database import DB
+from main.mainApps import App
+from main.Registration import Registr
 from main.styledWidgets import EntryWithPlaceholder, HoverButton
-
-
-# Окно с приложениями
-class App(Frame):
-    def __init__(self, parent, db):
-        # Объявление переменных
-        self.parent = parent
-        self.db = db
-        self.width = 28
-        self.height = 3
-        self.font = ("Courier", 30)
-
-        # Создание структуры окна
-        # Надпись
-        self.label = Label(
-            self.parent,
-            text='Приложения',
-            font=("Courier", 56)
-        )
-        # Все кнопки
-        self.Buttons = Frame()
-        self.Equation = HoverButton(
-            self.Buttons,
-            text="Решение уравнений",
-            font=self.font,
-            width=self.width,
-            height=self.height,
-            description=True,
-            descriptionText='Это делает Саня',
-            activebackground='#00ff00'
-        )
-        self.MatrixCalculator = HoverButton(
-            self.Buttons,
-            text="Матричный калькулятор",
-            font=self.font,
-            width=self.width,
-            height=self.height,
-            description=True,
-            descriptionText='Это делает Саня',
-            activebackground='#00ff00'
-        )
-        self.Calculator = HoverButton(
-            self.Buttons,
-            text="Арифметический калькулятор",
-            font=self.font,
-            width=self.width,
-            height=self.height,
-            description=True,
-            descriptionText='Это делает Витя',
-            activebackground='#00ff00'
-        )
-        self.RandomDigits = HoverButton(
-            self.Buttons,
-            text="Генератор случайных чисел",
-            font=self.font,
-            width=self.width,
-            height=self.height,
-            description=True,
-            descriptionText='Кто это делает?',
-            activebackground='#00ff00'
-        )
-        self.CalculatorSystemDigit = HoverButton(
-            self.Buttons,
-            text="Калькулятор перевода СС",
-            font=self.font,
-            width=self.width,
-            height=self.height,
-            description=True,
-            descriptionText='Кто это делает?',
-            activebackground='#00ff00'
-        )
-        self.CalculatorSystemUnits = HoverButton(
-            self.Buttons,
-            text="Калькулятор перевода единиц",
-            font=self.font,
-            width=self.width,
-            height=self.height,
-            description=True,
-            descriptionText='Кто это делает?',
-            activebackground='#00ff00'
-        )
-        self.TruthDiagram = HoverButton(
-            self.Buttons,
-            text="Калькулятор перевода единиц",
-            font=self.font,
-            width=self.width,
-            height=self.height,
-            description=True,
-            descriptionText='Кто это делает?',
-            activebackground='#00ff00'
-        )
-        # Раположение кнопок в сетку
-        self.Equation.grid(row=0, column=0, padx=(50, 30), pady=10)
-        self.MatrixCalculator.grid(row=0, column=1, padx=(30, 50), pady=10)
-        self.Calculator.grid(row=1, column=0, padx=(50, 30), pady=10)
-        self.RandomDigits.grid(row=1, column=1, padx=(30, 50), pady=10)
-        self.CalculatorSystemDigit.grid(
-            row=2,
-            column=0,
-            padx=(50, 30),
-            pady=10
-        )
-        self.CalculatorSystemUnits.grid(
-            row=2,
-            column=1,
-            padx=(30, 50),
-            pady=10
-        )
-        self.TruthDiagram.grid(row=3, column=0, padx=(50, 30), pady=10)
-        # Вывод на экран
-        self.label.pack()
-        self.Buttons.pack()
-        # Изменение цвета
-        self.parent['bg'] = '#00ace6'
-        for wid in self.parent.winfo_children():
-            wid.configure(bg='#00ace6')
 
 
 # Окно авторизации
 class Authentication(Frame):
     def __init__(self, parent, db):
         Frame.__init__(self)
+        # Создание основных переменных
         self.db = db
         self.parent = parent
         self.loggined = False
         self.user = None
+        # Создание основного фрейма
         self.auth = Frame()
+        # Название
         self.label = Label(
             self.auth,
             text='Авторизация',
             font=("Courier", 44)
         )
-        self.label['bg'] = '#00ace6'
         self.label.pack()
+        # Логин и пароль
         self.Login = StringVar()
         self.Password = StringVar()
+        # Поле ввода для логина и пароля
         self.login = EntryWithPlaceholder(
             self.auth,
             'Логин',
@@ -153,6 +42,7 @@ class Authentication(Frame):
             font=("Courier", 44)
         )
         self.password.pack(pady=15)
+        # Кнопка подтверждения
         self.submit = HoverButton(
             self.auth,
             text='Подтвердить',
@@ -161,6 +51,7 @@ class Authentication(Frame):
             activebackground='#00ff00'
         )
         self.submit.pack(pady=(0, 15))
+        # Кнопка для регистрации
         self.reg = HoverButton(
             self.auth,
             text="Зарегистрироваться",
@@ -169,6 +60,8 @@ class Authentication(Frame):
             activebackground='#00ff00'
         )
         self.reg.pack()
+        # Изменение цвета
+        self.label['bg'] = '#00ace6'
         for wid in self.parent.winfo_children():
             wid.configure(bg='#00ace6')
         self.auth.pack(pady=150)
@@ -198,85 +91,20 @@ class Authentication(Frame):
         self.app = Registr(self.newWindow, self.db)
 
 
-# Окно регистрации
-class Registr(Frame):
-    def __init__(self, parent, db):
-        self.parent = parent
-        self.parent.geometry('1200x800')
-        self.parent.title("Регистрация")
-        self.db = db
-        self.reg = Frame(self.parent)
-        self.label = Label(
-            self.reg,
-            text='Регистрация',
-            font=("Courier", 44)
-        )
-        self.label.pack()
-        self.label['bg'] = '#00ace6'
-        self.Login = StringVar()
-        self.Password = StringVar()
-        self.login = EntryWithPlaceholder(
-            self.reg,
-            'Логин',
-            font=("Courier", 44),
-            textvariable=self.Login
-        )
-        self.login.pack()
-        self.password = EntryWithPlaceholder(
-            self.reg,
-            'Пароль',
-            font=("Courier", 44),
-            hideChar=True,
-            textvariable=self.Password,
-        )
-        self.password.pack(pady=15)
-        self.submit = HoverButton(
-            self.reg,
-            text='Подтвердить',
-            font=("Courier", 44),
-            command=lambda: self.addToDb(
-                self.login.get(),
-                self.password.get()
-            ),
-            activebackground='#00ff00'
-        )
-        self.submit.pack()
-        self.parent['bg'] = '#00ace6'
-        self.parent.resizable(width=False, height=False)
-        self.parent.attributes('-fullscreen', True)
-        for wid in self.parent.winfo_children():
-            wid.configure(bg='#00ace6')
-        self.reg.pack(pady=150)
-
-    # Добавление в базу данных и проверха возможности добавления
-    def addToDb(self, login, password):
-        if self.db.checkUserLogin(login):
-            messagebox.showerror(
-                "Регистрация",
-                "Пользователь уже существует"
-            )
-            self.parent.attributes('-topmost', True)
-            self.parent.update()
-        else:
-            self.db.insertData(login, password)
-            messagebox.showinfo(
-                "Регистрация",
-                "Пользователь успешно зарегистрирован"
-            )
-            self.parent.attributes('-topmost', True)
-            self.parent.update()
-            self.parent.destroy()
-
-
 # Создание приложения
 def create_app():
+    # Создание и вызов базы данных
     db = DB()
+    # Главное окно
     root = Tk()
+    # Изменение параметров окна
     root.title('Авторизация')
     root.geometry("1200x800")
     root['bg'] = '#00ace6'
     root.resizable(width=False, height=False)
     root.attributes('-fullscreen', True)
+    # root.wm_state('zoomed')
+    # Вызов приложения
     app = Authentication(root, db)
     app.pack()
     root.mainloop()
--- main/styledWidgets.py
+++ main/styledWidgets.py
@@ -1,27 +1,33 @@
 from tkinter import *
 
 
-# Измененное поле ввода
+# Измененное поле ввода c подсказками
 class EntryWithPlaceholder(Entry):
     def __init__(self, master=None, placeholder=None, hideChar=False, **p):
-        super().__init__(master, p)
+        super().__init__(master, **p)
         self.master = master
+        # Символы для пароля
         self.hideChar = hideChar
 
+        # Если есть подсказка
         if placeholder is not None:
             self.placeholder = placeholder
             self.placeholder_color = 'grey'
             self.default_fg_color = self['fg']
 
+            # Вызов функций при нажатии и отжатии поля ввода
             self.bind("<FocusIn>", self.focus_in)
             self.bind("<FocusOut>", self.focus_out)
 
             self.put_placeholder()
 
+    # Надпись в поле ввода
     def put_placeholder(self):
+        self.delete(0, 'end')
         self.insert(0, self.placeholder)
         self['fg'] = self.placeholder_color
 
+    # Метод для очистки поля и замены сиволов при надобности
     def focus_in(self, *args):
         if self['fg'] == self.placeholder_color:
             self.delete('0', 'end')
@@ -30,7 +36,9 @@ class EntryWithPlaceholder(Entry):
                 self.config(show='*')
                 self.master.update()
 
+    # Метод для вывода подсказки
     def focus_out(self, *args):
+        # Если в поле пусто
         if not self.get():
             self.put_placeholder()
             self.config(show='')
@@ -41,18 +49,25 @@ class HoverButton(Button):
     def __init__(self, master, description=False, descriptionText='', **kw):
         Button.__init__(self, master=master, overrelief=GROOVE, **kw)
         self.defaultBackground = self["background"]
+        # Перемення для проверки есть ли пояснение
         self.description = description
         if self.description:
+            # Виджет для пояснения
             self.toolTip = ToolTip(self)
+        # Текст пояснение
         self.descriptionText = descriptionText
+        # Метод при наведии
         self.bind("<Enter>", self.on_enter)
+        # Метод при отведении курсора от кнопки
         self.bind("<Leave>", self.on_leave)
 
+    # Пояснение при наведении
     def on_enter(self, e):
         self['background'] = self['activebackground']
         if self.description:
             self.toolTip.showtip(self.descriptionText)
 
+    # Удаление пояснения
     def on_leave(self, e):
         self['background'] = self.defaultBackground
         if self.description:
@@ -65,21 +80,26 @@ class ToolTip(object):
         self.widget = widget
         self.tipwindow = None
 
+    # Создание текста
     def showtip(self, text):
         self.text = text
         if self.tipwindow or not self.text:
             return
+        # Координаты относительно кнопки
         x, y, cx, cy = self.widget.bbox("insert")
         x = x + self.widget.winfo_rootx() + 187
         y = y + cy + self.widget.winfo_rooty() + 127
+        # Вывести над всеми окнами
         self.tipwindow = tw = Toplevel(self.widget)
         tw.wm_overrideredirect(1)
         tw.wm_geometry("+%d+%d" % (x, y))
+        # Вывод поля на экран
         label = Label(tw, text=self.text, justify=LEFT,
                       background="#ffffe0", relief=SOLID, borderwidth=1,
                       font=("tahoma", "20", "normal"))
         label.pack(ipadx=1)
 
+    # Скрыть описание
     def hidetip(self):
         tw = self.tipwindow
         self.tipwindow = None
